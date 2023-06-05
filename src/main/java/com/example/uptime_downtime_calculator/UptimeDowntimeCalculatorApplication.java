000ec6d614ba49142c8d48d09ac63f63fe16be99
YOUR NAME <YOUR_EMAIL>
2023-06-05 18:25:49 +0200

    add first commit

---
+++ src/main/java/com/example/uptime_downtime_calculator/UptimeDowntimeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.uptime_downtime_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptimeDowntimeCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UptimeDowntimeCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uptime_downtime_calculator/UptimeDowntimeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uptime_downtime_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptimeDowntimeCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
